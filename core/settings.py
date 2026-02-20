e68cc809cb2a943f0dc68b86e6eb2f02c1d468ca
Pedro Arthur Magalhães Alecrim <118025418+pedroalec@users.noreply.github.com>
2026-02-20 16:30:07 -0300

    Removendo o protocolo(https) do dominio adicionado em ALLOWED_HOST

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.0.31", "doc.mdr.local", "https://doc.mdradvocacia.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.0.31", "doc.mdr.local", "doc.mdradvocacia.com"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000", 
